692fc59297005d32d26ed57a38dd5faa05e0fa9f
oguhpereira <oguhpereira@protonmail.com>
2022-08-29 22:32:56 -0300

    feat: remove option when disable plugin

--- src/Rocket.Chat-livechat/includes/class-rocketchat-livechat-deactivator.php
+++ src/Rocket.Chat-livechat/includes/class-rocketchat-livechat-deactivator.php
@@ -1,29 +1,10 @@
 <?php
 
-/**
- * Fired during plugin deactivation
- *
- * @link       http://rocket.chat
- * @since      1.0.0
- *
- * @package    Rocketchat_Livechat
- * @subpackage Rocketchat_Livechat/includes
- */
-
-/**
- * Fired during plugin deactivation.
- *
- * This class defines all code necessary to run during the plugin's deactivation.
- *
- * @since      1.0.0
- * @package    Rocketchat_Livechat
- * @subpackage Rocketchat_Livechat/includes
- * @author     Marko Banušić <mbanusic@gmail.com>
- */
-class Rocketchat_Livechat_Deactivator {
+class RocketChatLivechat_Deactivator {
 
 	public static function deactivate() {
 		//currently do nothing
+		delete_option('rocketchat-livechat-url');
 	}
 
 }
